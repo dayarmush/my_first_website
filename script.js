06d9f539acce8208a591e6a616175ef97c3de88d
David Yarmush <yarmushda@gmail.com>
2023-03-10 10:48:37 -0500

    small updates, event lintener in script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content is added by JavaScript";
+h2.textContent = "Brought to you by Dovid and JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const button = document.getElementById('infotag');
+button.addEventListener('click', function() {
+    button.style.display = 'none'
+});
